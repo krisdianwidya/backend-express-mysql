33f8436be46a7af5ea8b5f2314768c454570daa9
Krisdian Widya Ekatama <krisdianwidya@gmail.com>
2024-05-22 22:02:54 +0700

    update code to allow all cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const PORT = process.env.PORT || 5000
 const express = require('express');
+const cors = require('cors')
 
 const employeesRoutes = require('./routes/employees')
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.use('/employees', employeesRoutes)
 
